ddd7bde59ff1ba285f25c9ad2d8b71f62e2a8406
Muhammed Ahmed <muhammed.ahmed@bridgemarq.com>
2023-09-05 12:45:28 -0400

    testing checkin from desktop

--- public/src/index.js
+++ public/src/index.js
@@ -1,5 +1,5 @@
 console.log('starting client.......')
-const view = '<H1>Hello World!....123</H1>'
+const view = '<H1>Hello World!....1234</H1>'
 
 const divMain = document.getElementById('divMain');
 if(divMain) {
