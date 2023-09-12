a56c36fbf6e1295c46e3241feb520f2c771b0daa
Muhammed Ahmed <muhammed.ahmed@bridgemarq.com>
2023-09-12 07:48:41 -0400

    add batch route to create thumbnails in batch

--- src/index.js
+++ src/index.js
@@ -21,6 +21,7 @@ app.get('/url/metadata/*', f1);
 app.get('/url/thumb*', f2);
 app.get('/url/image/*', f3);
 app.get('/url/folder/*', f4);
+app.get('/url/batch/*', f5);
 
 async function f1(req, res, next){
     debug('\n\n\n\nIn function f1.....');
@@ -56,19 +57,56 @@ async function f4(req, res, next){
     if(b64fn=='zzz')
     {
         folderPath  = '//BRESDSK-MAHMED/Data';
-    
+
     }
     else if(b64fn=='')
     {
         folderPath  = '//BRESDSK-MAHMED/Data/Pictures/Equations';
-    
+
     }
     else
     {
         const filename = Utf8Base64Converter.decodeFromBase64(b64fn);
-        folderPath  = folderPath + '/' + filename;    
+        folderPath  = folderPath + '/' + filename;
     }
     console.log('folderPath', folderPath)
     console.log('redirecting.....');
     res.redirect('/');
+}
+
+
+async function f5(req, res, next){
+    debug('\n\n\n\nIn function f5.....');
+    const b64fn = req.path.replace('/url/batch/','');
+    const folderpath = Utf8Base64Converter.decodeFromBase64(b64fn) + '/';
+    debug(b64fn, folderpath);
+
+    let x = await f6(folderpath);
+    // debug('dddddddddddddddddddddddddddddddddddddddddddddddddddd');
+    // debug(x);
+    // let i=0;
+    // for(i=0; i<x.length; i++)
+    // {
+    //     if(x[i].isDir){
+    //         debug(x[i].fn);
+    //         f6(folderpath + x[i].fn + '/');
+    //     }
+    // }
+}
+
+async function f6(folderpath){
+    debug('\n\n\n\nIn function f6.....' + folderpath);
+    if((await fl.createMetaData(folderpath)) === true) {
+        await fl.createThumbnail(folderpath);
+
+        const x= fl.readJSON(folderpath + 'metadata.json');
+        for(let i=0; i<x.length; i++)
+        {
+            if(x[i].isDir){
+                debug(x[i].fn);
+                await f6(folderpath + x[i].fn + '/');
+            }
+        }
+    }
+    return null;
 }
